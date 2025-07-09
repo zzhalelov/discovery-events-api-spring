9abe457f18bf51c0cc0183ef330c74df55586384
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-07-09 23:35:10 +0500

    fix: change endpoint

--- events-service/src/main/java/kz/zzhalelov/server/user/UserController.java
+++ events-service/src/main/java/kz/zzhalelov/server/user/UserController.java
@@ -8,9 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("/admin/users")
 public class UserController {
     private final UserMapper userMapper;
     private final UserService userService;
@@ -20,5 +23,17 @@ public class UserController {
     public UserResponseDto create(@RequestBody @Valid UserCreateDto userCreateDto) {
         User user = userMapper.fromCreate(userCreateDto);
         return userMapper.toResponse(userService.create(user));
+    }
+
+    @GetMapping
+    public List<UserResponseDto> findAll() {
+        return userService.findAll().stream()
+                .map(userMapper::toResponse)
+                .collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/{userId}")
+    public void deleteById(@PathVariable int userId) {
+        userService.delete(userId);
     }
 }
