73c3167471a884bf8782d12d38f38ba521b310c9
Zhastilek Zhalelov <zzhalelov@gmail.com>
2025-08-02 20:00:38 +0500

    feat: create dtos for events

---
+++ events-service/src/main/java/kz/zzhalelov/server/event/dto/AdminEventUpdateDto.java
@@ -0,0 +1,32 @@
+package kz.zzhalelov.server.event.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
+import kz.zzhalelov.server.event.eventEnum.AdminStateAction;
+import kz.zzhalelov.server.event.eventEnum.PublicStateAction;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AdminEventUpdateDto {
+    @Size(min = 20, max = 2000)
+    String annotation;
+    Long category;
+    @Size(min = 20, max = 7000)
+    String description;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    LocalDateTime eventDate;
+    LocationDto location;
+    Boolean paid;
+    @Min(0)
+    Integer participantLimit;
+    Boolean requestModeration;
+    @Size(min = 3, max = 120)
+    String title;
+    AdminStateAction state;
+}
